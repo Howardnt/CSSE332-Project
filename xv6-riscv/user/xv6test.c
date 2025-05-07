a0518c6de729e59c41c0efbe3b086a0a73f1fcd1
Howardnt <howardnt@rose-hulman.edu>
2025-05-07 18:57:16 -0400

    added initial :)

---
+++ xv6-riscv/user/xv6test.c
@@ -0,0 +1,20 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[]) {
+
+  printf("SPOON TEST:\n");
+  uint64 p = 0xdeadbeef;
+  spoon((void*)p);
+
+  printf("THREAD_CREATE TEST:\n");
+  uint64 t = 0xabcdefab;
+  uint64 a = 0xbafedcba;
+  thread_create((void*)t, (void*)p, (void*)a);
+
+  printf("THREAD_COMBINE TEST:\n");
+  thread_combine((void*)t);
+
+  exit(0);
+}
