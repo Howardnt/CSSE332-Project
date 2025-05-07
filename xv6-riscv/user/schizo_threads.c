057c0ce90280ece6498a609b0e9aec30adcf23e7
Atli Arnarsson <arnarsan@rose-hulman.edu>
2025-05-07 19:27:28 -0400

    Added sthread API

--- xv6-riscv/user/schizo_threads.c
+++ xv6-riscv/user/schizo_threads.c
@@ -1,3 +1,20 @@
+#include "user/user.h"
+
+typedef struct {
+
+} sthread_t;
+
+typedef void * (*generic_fn_ptr_t)(void *);
+
+
+int sthread_create(sthread_t *thread, generic_fn_ptr_t fn, void *arg) {
+  return 1; // TODO
+}
+
+int sthread_join(sthread_t *thread) {
+  return 0;
+}
+
 int main() {
   return 0; // dummy main
 }
