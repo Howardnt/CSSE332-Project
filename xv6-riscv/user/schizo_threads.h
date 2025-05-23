19a8c2e1ce9f876011a6cb4c7a7520a838fd0f31
Jack Traversa <traverjj@rose-hulman.edu>
2025-05-23 00:05:25 -0400

    FINAL FINAL

--- xv6-riscv/user/schizo_threads.h
+++ xv6-riscv/user/schizo_threads.h
@@ -10,22 +10,9 @@
 typedef int sthread_t;
 typedef void(*sthread_fn_in_t)(void *);
 
-int sthread_create(
-		sthread_t *thread, 
-		sthread_fn_in_t fn,
-		void *args, 
-		void *stack);
 
-<<<<<<< HEAD
 int sthread_create(sthread_t *thread, sthread_fn_in_t fn, void *args, void *stack);
-uint64 sthread_join(sthread_t *thread);
-int sthread_exit(void* arg);
-=======
-
 void *sthread_join(sthread_t *thread);
-
-
 int sthread_exit();
->>>>>>> ce1c0818ef16896487c77ff85b0d1720a2f750d7
 
 #endif // __SCHIZO_THREADS_H
